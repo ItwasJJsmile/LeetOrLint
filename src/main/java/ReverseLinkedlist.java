9eeaa628fd526b5cf70fb3450fe8c4050359c950
ItwasJJsmile <ItwasJJsmile@gmail.com>
2019-02-27 22:36:07 +0800

    first commit

---
+++ src/main/java/ReverseLinkedlist.java
@@ -0,0 +1,12 @@
+public class ReverseLinkedlist {
+        public ListNode reverseList(ListNode head) {
+            return null;
+        }
+}
+
+// Definition for singly-linked list.
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
